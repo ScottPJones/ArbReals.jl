2953b52119c4fb5bc883228ece2e618471c12c12
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-07-05 06:46:06 -0400

    add refs

--- src/ArbTypes/ArbCstructs.jl
+++ src/ArbTypes/ArbCstructs.jl
@@ -1,5 +1,13 @@
 #=
     types that mirror the memory layout of C structs used in Arb
+    
+   The mag type used by Arb (fredrikj.net/arb/mag.html)
+   The arf type used by Arb (fredrikj.net/arb/arf.html)
+   The arb type used by Arb (fredrikj.net/arb/arb.html)
+   see also (https://github.com/Nemocas/Nemo.jl/blob/master/src/arb/ArbTypes.jl)
+   see also (https://github.com/thofma/Hecke.jl/blob/master/src/Misc/mag.jl)
+   see also (https://github.com/thofma/Hecke.jl/blob/master/src/Misc/arf.jl)
+   see also (https://github.com/thofma/Hecke.jl/blob/master/src/Misc/arb.jl)
 =#
 
 
